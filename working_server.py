8055d47ef01d1e24f7d10983f4724a8ae22eefc0
skatinger <alex.nyffenegger@outlook.com>
2020-10-03 15:22:52 +0200

    color observation works

--- working_server.py
+++ working_server.py
@@ -6,6 +6,8 @@ import asyncio
 import aiocoap.resource as resource
 import aiocoap
 import datetime
+import json
+import random
 from aiohttp import web
 
 
@@ -28,19 +30,83 @@ class CustomResource(resource.Resource):
     def set_content(self, content):
         self.content = content
 
-    async def render_get(self, request):
-        print("woow, cool i received a GET request")
-        return aiocoap.Message(payload=self.content)
+    # async def render_get(self, request):
+    #     print("woow, cool i received a GET request")
+    #     return aiocoap.Message(payload=self.content)
 
     async def render_put(self, request):
-        print('===== PUT payload: %s' % request.payload)
-        self.set_content(request.payload.decode("utf-8") )
+        print('===== PUT payload: %s' % request.payload.decode("utf-8"))
+        self.set_content( request.payload.decode("utf-8") )
         return aiocoap.Message(code=aiocoap.CHANGED, payload=self.content)
 
-async def button_observe(request):
-    print("BUTTON OBSERVE triggered with request:")
-    print(request)
-    return aiocoap.Message(payload="cool")
+
+class LedResource(resource.ObservableResource):
+    """Example resource that can be observed. The `notify` method keeps
+    scheduling itself, and calles `update_state` to trigger sending
+    notifications."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.handle = None
+        self.color = "ffffff"
+
+    def notify(self):
+        self.update_resource()
+        self.updated_state()
+        self.reschedule()
+
+    def update_resource(self):
+        colors = ["ff0000", "ebe134", "abcdef", "ffffff", "gggggg"]
+        self.color = random.choice(colors)
+
+    # during testing we call this, once the server runs we send messages from http_app
+    # to trigger a notify instead of calling it here
+    def reschedule(self):
+        self.handle = asyncio.get_event_loop().call_later(5, self.notify)
+
+    def update_observation_count(self, count):
+        if count and self.handle is None:
+            self.reschedule()
+
+
+    async def render_get(self, request):
+        print("COLOR IS:")
+        print("sdfasdfasdf" + self.color)
+        payload = ("{\"appId\":\"LED\",\"data\":{\"color\":\"" + self.color + "\"},\"messageType\":\"CFG_SET\"}").encode("utf-8")
+        return aiocoap.Message(payload=payload)
+
+# used for testing, works
+class TimeResource(resource.ObservableResource):
+    """Example resource that can be observed. The `notify` method keeps
+    scheduling itself, and calles `update_state` to trigger sending
+    notifications."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.handle = None
+
+    def notify(self):
+        self.updated_state()
+        self.reschedule()
+
+    def reschedule(self):
+        self.handle = asyncio.get_event_loop().call_later(5, self.notify)
+
+    def update_observation_count(self, count):
+        if count and self.handle is None:
+            print("Starting the clock")
+            self.reschedule()
+        if count == 0 and self.handle:
+            print("Stopping the clock")
+            self.handle.cancel()
+            self.handle = None
+
+    async def render_get(self, request):
+        payload = datetime.datetime.now().\
+                strftime("%Y-%m-%d %H:%M").encode('ascii')
+        return aiocoap.Message(payload=payload)
 
 # logging setup
 logging.basicConfig(level=logging.INFO)
@@ -62,7 +128,9 @@ root = resource.Site()
 root.add_resource(['.well-known', 'core'],
         resource.WKCResource(root.get_resources_as_linkheader))
 root.add_resource(['testing'], CustomResource())
-root.add_resource(['button_observe'], button_observe)
+root.add_resource(['led'], LedResource())
+root.add_resource(['time'], TimeResource())
+
 # creates a context to all addresses on the default coap port
 asyncio.Task(aiocoap.Context.create_server_context(root))
 
